09ca4e33ce87ac6c91b3957e3ff366dda985ba8b
Ariful Haque <arifulhaque2010@yahoo.com>
2018-01-10 23:37:01 -0500

    Create basic app file

---
+++ app/app1.py
@@ -0,0 +1,16 @@
+import time
+def square(numbers):
+  print("Print squares")
+  for n in numbers:
+    time.sleep(0.2)
+    print("Square", n*n)
+def cube(numbers):
+  print("Print cubes")
+  for n in numbers:
+    time.sleep(0.2)
+    print("Cube", n*n*n)
+arr = [2,3,4,5]
+t = time.time()
+square(arr)
+cube(arr)
+print("Program took", time.time()-t)
