bb474cd4b96c7e1bf59d7ab6d3b307b458494103
Ariful Haque <arifulhaque2010@yahoo.com>
2018-01-12 12:50:26 -0500

    Add oop function

--- app/add_number.py
+++ app/add_number.py
@@ -1,6 +1,8 @@
-"""Write a program that adds two numbers and prints the result"""
+# Write a program that adds two numbers and prints the result
+
 
 import threading 
+
 
 def uf(num1, num2):
 	"""This is user defined thread function"""
@@ -8,7 +10,9 @@ def uf(num1, num2):
 	print "Result = %d" %(int(num1)+int(num2))
 	return
 
+def Main():
+	t = threading.Thread(target=uf, args=(12,13))
+	t.start()
 
 if __name__ == '__main__':
-	t = threading.Thread(target=uf, args=(2,3))
-	t.start()
+	Main()
