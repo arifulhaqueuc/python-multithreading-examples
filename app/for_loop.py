7966ce626f688836a11f62a8eddda91a4558201b
Ariful Haque <arifulhaque2010@yahoo.com>
2018-01-20 10:46:45 -0500

    Add options with for loop program

--- app/for_loop.py
+++ app/for_loop.py
@@ -7,9 +7,21 @@ def uf():
 	print "Hello World"
 	return
 
+
+## 1st apprach to get the for loop running
 if __name__ == "__main__":
 	threads = [] # Threads list needed when we use a bulk of threads
 	for i in range(5):
 		t = threading.Thread(target=uf)
 		threads.append(t)
 		t.start()
+
+
+## 2nd approach would be as follows
+"""
+threads = []
+for i in range(5):
+	t = threading.Thread(target=uf)
+	threads.append(t)
+	t.start()
+"""
