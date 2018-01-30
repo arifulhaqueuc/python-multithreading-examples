79e68158ec00a97796f60d403b2cd03774814526
Ariful Haque <arifulhaque2010@yahoo.com>
2018-01-30 18:05:07 -0500

    Add program on factorial using threading

---
+++ app/script_18.py
@@ -0,0 +1,22 @@
+##1. Calculate factorial using recursion.
+##2. Call factorial function using thread. 
+
+from thread import start_new_thread
+
+threadId = 1
+
+def factorial(n):
+   global threadId
+   if n < 1:   # base case
+       print "%s: %d" % ("Thread", threadId )
+       threadId += 1
+       return 1
+   else:
+       returnNumber = n * factorial( n - 1 )  # recursive call
+       print(str(n) + '! = ' + str(returnNumber))
+       return returnNumber
+
+start_new_thread(factorial,(5, ))
+start_new_thread(factorial,(4, ))
+
+c = raw_input("Waiting for threads to return...\n")
