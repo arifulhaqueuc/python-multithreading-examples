62b44d56437b0bf7236ee68acccbc525e8fedb98
Ariful Haque <arifulhaque2010@yahoo.com>
2018-01-30 13:19:18 -0500

    Create thread function with args

---
+++ app/script14.py
@@ -0,0 +1,16 @@
+import time
+from threading import Thread
+
+def ThreadFunction(x):
+    print "sleeping 5 sec from thread %d" % x
+    time.sleep(5)
+    print "finished sleeping from thread %d" % x
+
+def Main():
+  for i in range(10):
+      myThread = Thread(target=ThreadFunction, args=(i,))
+      myThread.start()
+      
+if __name__ == "__main__":
+  Main()
+  
