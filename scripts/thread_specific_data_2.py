d6a40861dfa5678e299bdfc3f49fc8a203a5f3f2
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-06 12:57:39 -0500

    Create thread_specific_data_2.py

---
+++ scripts/thread_specific_data_2.py
@@ -0,0 +1,34 @@
+import random
+import threading
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-10s) %(message)s',
+                    )
+
+
+def show_value(data):
+    try:
+        val = data.value
+    except AttributeError:
+        logging.debug('No value yet')
+    else:
+        logging.debug('value=%s', val)
+
+def worker(data):
+    show_value(data)
+    data.value = random.randint(1, 100)
+    show_value(data)
+
+class MyLocal(threading.local):
+    def __init__(self, value):
+        logging.debug('Initializing %r', self)
+        self.value = value
+
+local_data = MyLocal(1000)
+show_value(local_data)
+
+for i in range(2):
+    t = threading.Thread(target=worker, args=(local_data,))
+    t.start()
+    
