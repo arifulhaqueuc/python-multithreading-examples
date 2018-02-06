1b4660b76df774e111c18aea827c2aa1e631721f
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-06 12:48:47 -0500

    Create threading_daemon.py

---
+++ scripts/threading_daemon.py
@@ -0,0 +1,28 @@
+
+import threading
+import time
+import logging
+
+
+def daemon():
+    logging.debug('Starting')
+    time.sleep(0.2)
+    logging.debug('Exiting')
+
+
+def non_daemon():
+    logging.debug('Starting')
+    logging.debug('Exiting')
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='(%(threadName)-10s) %(message)s',
+)
+
+d = threading.Thread(name='daemon', target=daemon, daemon=True)
+
+t = threading.Thread(name='non-daemon', target=non_daemon)
+
+d.start()
+t.start()
